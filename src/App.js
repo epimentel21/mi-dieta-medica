1d34dd8d24264df4cd13e23a51d1d448d18ccb16
epimentel21 <epimentel21@gmail.com>
2026-05-12 15:58:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,3 +8,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLahLbWHq36JMrOgprctCC7TdwCFZsRpI",
+  authDomain: "nutricloud-pro.firebaseapp.com",
+  projectId: "nutricloud-pro",
+  storageBucket: "nutricloud-pro.firebasestorage.app",
+  messagingSenderId: "235206653188",
+  appId: "1:235206653188:web:bc3e0a9d9eda676c340c51",
+  measurementId: "G-9G01TNQ3S2"
+};
